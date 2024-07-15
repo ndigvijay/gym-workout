8c85639c9b1baab560098b11f9e0287fe9f2cc59
Digvijay Narayan <digvijayn1993@gmail.com>
2024-07-15 19:18:00 +0530

    completed signup controller

--- services/auth/controllers/signup.go
+++ services/auth/controllers/signup.go
@@ -1,11 +1,16 @@
 package controllers
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 	"github.com/ndigvijay/gym-workout/services/auth/db"
 	"github.com/ndigvijay/gym-workout/services/auth/models"
+
 	// "log"
 	"net/http"
+
+	"github.com/alexedwards/argon2id"
 )
 
 func Signup(c *gin.Context) {
@@ -18,10 +23,20 @@ func Signup(c *gin.Context) {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Invalid request payload"})
 		return
 	}
+	var existingUser models.User
 
-	newUser := models.User{
+	if existingUserError:=db.DB.Where("username=?",body.Username).First(&existingUser);existingUserError.Error==nil{
+		c.JSON(402,gin.H{
+			"messsage":"user already exists",
+		})
+		return
+	}	
+	hash,err:=argon2id.CreateHash(body.Password,argon2id.DefaultParams);if err!=nil{
+		log.Fatal(err)
+	}
+	newUser:= models.User{
 		Username: body.Username,
-		Password: body.Password,
+		Password: hash,
 	}
 
 	result := db.DB.Create(&newUser)
@@ -29,7 +44,7 @@ func Signup(c *gin.Context) {
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
 		return
 	}
-
+	
 	c.JSON(http.StatusOK, gin.H{
 		"user": newUser,
 	})
--- services/auth/go.mod
+++ services/auth/go.mod
@@ -3,6 +3,7 @@ module github.com/ndigvijay/gym-workout/services/auth
 go 1.22.2
 
 require (
+	github.com/alexedwards/argon2id v1.0.0
 	github.com/gin-gonic/gin v1.10.0
 	github.com/joho/godotenv v1.5.1
 	gorm.io/driver/postgres v1.5.9
