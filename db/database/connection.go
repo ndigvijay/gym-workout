4c5ca39f4cf769b9fc5c91f0025aeb4e1265974e
Digvijay Narayan <digvijayn1993@gmail.com>
2024-07-15 08:59:50 +0530

    intial commit

---
+++ db/database/connection.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"sync"
+	"fmt"
+	"gorm.io/gorm"
+	"gorm.io/driver/postgres"
+	"log"
+	"os"
+)
+
+var DBLock *sync.Mutex
+
+func InitDB()*gorm.DB{
+	DBLock.Lock()
+	defer DBLock.Unlock()
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := 5432
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=require", host, user, password, dbname, port)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect database: %v", err)
+	}
+	return db
+}
---
+++ services/auth/go.mod
@@ -0,0 +1,3 @@
+module github.com/ndigvijay/gym-workout/services/auth
+
+go 1.22.2
